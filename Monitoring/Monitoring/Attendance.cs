626edaa14d7c9ed2e6c2409e4c3504dab1290aeb
Kenji Pinlac <134300371+reimo22@users.noreply.github.com>
2024-04-16 19:19:36 +0800

    pass attendanceStatus between forms
    from attendance from to class report
    uploaded tentative class diagram

--- Monitoring/Monitoring/Attendance.cs
+++ Monitoring/Monitoring/Attendance.cs
@@ -8,115 +8,137 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using GroupBox = System.Windows.Forms.GroupBox;
+using RadioButton = System.Windows.Forms.RadioButton;
 
 namespace Monitoring
 {
     public partial class Attendance : Form
     {
 
-/*
-public string[] students = {
-    "AGAN, Michael Christian",
-    "ALANGSAB, Riqueta",
-    "AÑASCO, Althea Zoe",
-    "AÑONUEVO, Chrysler",
-    "ATIM, Arielle Ershey",
-    "BAUTISTA, George Kiel",
-    "BECINA, Cyrill John",
-    "BIADOR, Mark Jacob",
-    "CABILAN, Rafael",
-    "CAPATI, Kentver",
-    "CASTILLO, Kristian Jerome",
-    "CONCEPCION, Lee Hendrix",
-    "DE GULA, Kerby Brent",
-    "DEL PRADO, Jerryco",
-    "DIAZANA, John Darren",
-    "DUMALAG, Jordan",
-    "EDILLON, Joshua Lloyd",
-    "ESCAÑO, Krisha Ann Ame",
-    "ESPAÑOL, Jem Theonie",
-    "GAPASIN, Michael Andrei",
-    "GARCIA, Enjo Mae",
-    "GRICO, Cirgs Alyxander",
-    "LEYESA, Dann Martin",
-    "LUZON, Adrian Dominic",
-    "MADIO, Jonalyn",
-    "MARTINEZ, Hero",
-    "MENDOZA, Wellhemstad",
-    "MERCADO, Nico",
-    "PANAGA, Charry",
-    "PINLAC, Kenji Luis",
-    "QUIJANO, Tim",
-    "QUILATAN, Marcis Joseph",
-    "REYNANCIA, Jessie Lei",
-    "RODRIGUEZ, Rhegille Gabriel",
-    "SAMACO, Cyrus",
-    "SILANG, Le Bron James",
-    "SIMON, Kielle Francez",
-    "SOLO, John",
-    "STA. ANA, Mary Angelique",
-    "TAGLE, Jel Kyann",
-    "VARELA, Venice Ariane",
-    "YABUT, Aleerah Marishka",
-    "ZAPATA, Kurt Anthony"
-        };
-        
+        /*
+        public string[] students = {
+            "AGAN, Michael Christian",
+            "ALANGSAB, Riqueta",
+            "AÑASCO, Althea Zoe",
+            "AÑONUEVO, Chrysler",
+            "ATIM, Arielle Ershey",
+            "BAUTISTA, George Kiel",
+            "BECINA, Cyrill John",
+            "BIADOR, Mark Jacob",
+            "CABILAN, Rafael",
+            "CAPATI, Kentver",
+            "CASTILLO, Kristian Jerome",
+            "CONCEPCION, Lee Hendrix",
+            "DE GULA, Kerby Brent",
+            "DEL PRADO, Jerryco",
+            "DIAZANA, John Darren",
+            "DUMALAG, Jordan",
+            "EDILLON, Joshua Lloyd",
+            "ESCAÑO, Krisha Ann Ame",
+            "ESPAÑOL, Jem Theonie",
+            "GAPASIN, Michael Andrei",
+            "GARCIA, Enjo Mae",
+            "GRICO, Cirgs Alyxander",
+            "LEYESA, Dann Martin",
+            "LUZON, Adrian Dominic",
+            "MADIO, Jonalyn",
+            "MARTINEZ, Hero",
+            "MENDOZA, Wellhemstad",
+            "MERCADO, Nico",
+            "PANAGA, Charry",
+            "PINLAC, Kenji Luis",
+            "QUIJANO, Tim",
+            "QUILATAN, Marcis Joseph",
+            "REYNANCIA, Jessie Lei",
+            "RODRIGUEZ, Rhegille Gabriel",
+            "SAMACO, Cyrus",
+            "SILANG, Le Bron James",
+            "SIMON, Kielle Francez",
+            "SOLO, John",
+            "STA. ANA, Mary Angelique",
+            "TAGLE, Jel Kyann",
+            "VARELA, Venice Ariane",
+            "YABUT, Aleerah Marishka",
+            "ZAPATA, Kurt Anthony"
+                };
 
-        
-         public string[] studentID = {"202320107",
-    "202312640",
-    "202311220",
-    "202313737",
-    "202311985",
-    "202311009",
-    "202314266",
-    "202311209",
-    "201811248",
-    "202313091",
-    "202310421",
-    "202310879",
-    "202312442",
-    "202311626",
-    "202313294",
-    "202310447",
-    "202311579",
-    "202310673",
-    "202314222",
-    "202312225",
-    "202311127",
-    "202312392",
-    "202312710",
-    "202312813",
-    "202311906",
-    "202313046",
-    "202310536",
-    "202313050",
-    "202116311",
-    "202310292",
-    "202313343",
-    "202313882",
-    "202311528",
-    "202119459",
-    "202313427",
-    "202311959",
-    "202311615",
-    "202310850",
-    "202313020",
-    "202313899",
-    "202310728",
-    "202312834",
-    "202312647" };
-        */
+
+
+                 public string[] studentID = {"202320107",
+            "202312640",
+            "202311220",
+            "202313737",
+            "202311985",
+            "202311009",
+            "202314266",
+            "202311209",
+            "201811248",
+            "202313091",
+            "202310421",
+            "202310879",
+            "202312442",
+            "202311626",
+            "202313294",
+            "202310447",
+            "202311579",
+            "202310673",
+            "202314222",
+            "202312225",
+            "202311127",
+            "202312392",
+            "202312710",
+            "202312813",
+            "202311906",
+            "202313046",
+            "202310536",
+            "202313050",
+            "202116311",
+            "202310292",
+            "202313343",
+            "202313882",
+            "202311528",
+            "202119459",
+            "202313427",
+            "202311959",
+            "202311615",
+            "202310850",
+            "202313020",
+            "202313899",
+            "202310728",
+            "202312834",
+            "202312647" };
+                */
         //FOR TEST CASE
-      public string[] students = { "Student1", "Student2", "Student3", "Student4", "Student5" };
-      public string[] studentID = { "ID1", "ID2", "ID3", "ID4", "ID5" };
-        private int[] studentStatus; 
+        public string[] students = { "Student1", "Student2", "Student3", "Student4", "Student5" };
+        public string[] studentID = { "ID1", "ID2", "ID3", "ID4", "ID5" };
+        public int[] attendanceStatus;
+        public DateTime date = new DateTime();
+        public int subject;
+        public Status attendance;
+
+        public class Status
+        {
+            public DateTime DateTimeStamp { get; set; }
+            public int[] AttendanceStatus { get; set; }
+            public int Subject { get; set; }
+            public Status(DateTime dateTimeStamp, int[] attendanceStatus, int subject)
+
+            {
+                DateTimeStamp = dateTimeStamp;
+                AttendanceStatus = attendanceStatus;
+                Subject = subject;
+            }
+
+        }
 
         public Attendance()
         {
             InitializeComponent();
             CreateGroupBoxes();
-            studentStatus = new int[students.Length];
+            attendanceStatus = new int[students.Length];
+            comboBox1.SelectedIndex = 0;
 
         }
         private void CreateGroupBoxes()
@@ -148,57 +170,34 @@ public string[] students = {
                         if (btn.Checked)
                         {
                             int index = (btn.Parent as GroupBox).TabIndex; // Get the index of the group box
-                            studentStatus[index] = Array.IndexOf(btn.Parent.Controls.OfType<RadioButton>().ToArray(), btn) + 1; // Store the selected index
+                            attendanceStatus[index] = Array.IndexOf(btn.Parent.Controls.OfType<RadioButton>().ToArray(), btn) + 1; // Store the selected index
+                            // 4 - Present, 3 - Absent, 2 - Late, 1 - Excused, 0 - No input
                         }
                     };
-          
+
                     groupBox.Controls.Add(radioButton);
                 }
                 flowLayoutPanel1.Controls.Add(groupBox);
                 groupBox.TabIndex = i;
             }
         }
+        List<int> selectedIndexList = new List<int>();
 
-     
-
-
-
-
-
-        private void submitAttendance_Click(object sender, EventArgs e)
+        private void SubmitAttendance(object sender, EventArgs e, Status status)
         {
 
             //FOR CHECKING
-            AllocConsole();
-            foreach (int selectedIndex in studentStatus)
+            /*AllocConsole();
+            foreach (int selectedIndex in attendanceStatus)
             {
                 Console.WriteLine(selectedIndex);
-            }
+            }*/
+
         }
 
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
@@ -270,6 +269,37 @@ public string[] students = {
 
         }
 
+        private void dateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+            date = dateTimePicker1.Value;
+        }
 
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            subject = comboBox1.SelectedIndex;
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void displayClassReport(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitAttendance_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Submitted successfully.");
+            attendance = new Status(date, attendanceStatus, subject);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            ClassReport classReport = new ClassReport();
+            classReport.GetData(students, studentID, attendance);
+            classReport.Show();
+        }
     }
 }
